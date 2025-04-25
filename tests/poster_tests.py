e0903ca5abbae2dc5351fff09ce3f2047a2b6da0
makl812c <makl812c@tu-dresden.de>
2025-04-25 11:03:15 +0200

    make selenium work

--- tests/poster_tests.py
+++ tests/poster_tests.py
@@ -11,6 +11,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class PythonOrgSearch(unittest.TestCase):
