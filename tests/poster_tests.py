bff4993015bb71d8f8bd75709f4452bc2691b4ad
makl812c <makl812c@tu-dresden.de>
2025-04-30 14:53:31 +0200

    added login tests

--- tests/poster_tests.py
+++ tests/poster_tests.py
@@ -5,8 +5,9 @@ import mypy
 import re
 from selenium import webdriver
 
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
 
@@ -21,7 +22,7 @@ class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
 
         if os.environ.get("GITHUB_ACTIONS"):
             options.binary_location = "/usr/bin/firefox"
@@ -37,32 +38,163 @@ class PythonOrgSearch(unittest.TestCase):
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get(f"http://{self.address}/scientific_poster_generator/login.php")
-        print("Hello World")
-        print(driver.title)
         self.assertIn("Poster Generator", driver.title)
+        print("Testing Page: " + driver.title)
 
-    def tearDown(self):
-        self.driver.close()
+        self.login_page(driver)
+        self.user_page(driver)
+        self.poster_page(driver, "")
+        self.admin_user(driver, "")
+        self.index_page(driver)
 
-    def login(self):
+        self.logout(driver)
+
+    # def tearDown(self):
+    #     self.driver.close()
+
+    def login_page(self, driver):
+
+        # check both empty
+        self.login_fill_form(driver, "", "")
+        driver.find_element(By.ID, "login").click()
+        self.assertEqual(
+            f"http://{self.address}/scientific_poster_generator/login.php",
+            driver.current_url,
+        )
+        self.login_clear_form(self.driver)
+
+        # check wrong name
+        self.login_fill_form(driver, "Max Mustermann" + "123", "AbC123-98xy")
+        driver.find_element(By.ID, "login").click()
+        self.assertEqual(
+            f"http://{self.address}/scientific_poster_generator/login.php",
+            driver.current_url,
+        )
+        self.login_clear_form(self.driver)
+
+        # check wrong pw
+        self.login_fill_form(driver, "Max Mustermann", "AbC123-98xy" + "abc")
+        driver.find_element(By.ID, "login").click()
+        self.assertEqual(
+            f"http://{self.address}/scientific_poster_generator/login.php",
+            driver.current_url,
+        )
+        self.login_clear_form(self.driver)
+
+        # check right name+pw
+        self.login_fill_form(driver, "Max Mustermann", "AbC123-98xy")
+        driver.find_element(By.ID, "login").click()
+        self.assertEqual(
+            "http://localhost/scientific_poster_generator/projects.php",
+            driver.current_url,
+        )
+
+        # check cookie session
+        session_id = driver.get_cookie("sessionID")
+        self.assertIsNotNone(session_id)
+
         pass
 
-    def logout(self):
+    def login_fill_form(self, driver, name, pw):
+        self.assertEqual(
+            f"http://{self.address}/scientific_poster_generator/login.php",
+            driver.current_url,
+        )
+        filed_name = driver.find_element(By.ID, "name")
+        filed_name.send_keys(name)
+        filed_name.send_keys(Keys.RETURN)
+
+        filed_pw = driver.find_element(By.ID, "pw")
+        filed_pw.send_keys(pw)
+        filed_pw.send_keys(Keys.RETURN)
+
+    def login_clear_form(self, driver):
+        driver.find_element(By.ID, "name").clear()
+        driver.find_element(By.ID, "pw").clear()
+
+    # TODO: unused
+    def logout(self, driver):
+        driver.get(f"http://{self.address}/scientific_poster_generator/projects.php")
+        # check if page contains logout
+        logout = driver.find_element(By.ID, "logout")
+        self.assertIsNotNone(logout)
+        logout.click()
+        self.assertEqual(
+            "http://localhost/scientific_poster_generator/login.php",
+            driver.current_url,
+        )
+
+        # check if correct logout   ???
         pass
 
-    def register(self):
+    def register(self, driver, name, pw):
+        # check existing name
+        # check invalid pw
+        # check two diffent pw
+        # check correct action + now at login page
         pass
 
-    def index_page(self):
+    def user_page(self, driver):
+        # check poster list correctly loaded
+        # check add new poster
+        #   check right date
+        # check edit poster title
+        # check delete new poster
+        # check access poster
+
+        # check author list correctly loaded
+        # check edit author name
+        # check delete author
+
+        # check image list  ???
         pass
 
-    def user_page(self):
+    def poster_page(self, driver, title):
+        # check right title
+        # check edit title
+        #   +globally
+        # check empty authors
+        # check add author
+        # check author list switch order
+        # check author stored
+        # check add box
+        #   +globally
+        # check basic edit box
+        # check box markdown render
+        # check box math render
+
+        # check box plotly render   ???
+        # check box image render    ???
+        #   + stored globally
+
+        # check visibility
+        #   - private
+        #   - public
+
+        # check right edit date
+        pass
+
+    def admin_user(self, driver, pw):
+        # check login successfully
+        # check posters set on public
+        # check set poster to visible
+        pass
+
+    def index_page(self, driver):
+        # check poster count
+        # check poster#1 content
+        # check prevent editing:
+        #   -title
+        #   -add author
+        #   -author item interaction
+        #   -change author order
+        #   -box text
+        #   -box image drop
+        #   -box interaction
         pass
 
 
 if __name__ == "__main__":
-    # print("Hello World")
-
     unittest.main()
 
     # TODO: test todo-to-issue test
