99d44ed1063cc870b25a07206c7349788c6f216a
makl812c <makl812c@tu-dresden.de>
2025-05-02 09:30:50 +0200

    fix page access issue

--- test.php
+++ test.php
@@ -3,6 +3,16 @@
     ini_set('display_errors', 1);
 
     echo "<p>PHP script is running!</p>";
+
+
+	$password = "password";
+	$servername = "localhost";
+	$username = "poster_generator";
+	$port = 3307;
+
+    $GLOBALS["conn"] = new mysqli($servername, $username, $password, "", $port);
+    $result = mysqli_query($$GLOBALS["conn"], "SELECT name FROM user;");
+    print_r($result);
 ?>
 <!DOCTYPE html>
 <html lang='en'>
