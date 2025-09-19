8c4d9163a4287a0290b46dbc062460483d0ea019
makl812c <makl812c@tu-dresden.de>
2025-09-19 15:44:22 +0200

    fixed issue #296

--- src/js/login.js
+++ src/js/login.js
@@ -1,32 +1,39 @@
-$(document).ready(function () {
-    document.getElementById("login-btn").onclick = function () {
+function login() {
+    const username = document.getElementById("name");
+    const password = document.getElementById("pw");
 
-        const username = document.getElementById("name");
-        const password = document.getElementById("pw");
-
-        if (username.value == "" || password.value == "") {
-            toastr["warning"]("Ensure you input a value in both fields!");
-        } else {
-            $.ajax({
-                type: "POST",
-                url: "/api/post_traffic.php",
-                data: {
-                    action: 'login',
-                    name: username.value,
-                    pw: password.value
-                },
-                success: function (response) {
-                    if (response == "Correct Password") {
-                        toastr["success"]("Logged in");
-                        window.location.href = "projects.php";
-                    } else {
-                        toastr["warning"](response);
-                    }
-                },
-                error: function () {
-                    toastr["error"]("An error occurred...");
+    if (username.value == "" || password.value == "") {
+        toastr["warning"]("Ensure you input a value in both fields!");
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/api/post_traffic.php",
+            data: {
+                action: 'login',
+                name: username.value,
+                pw: password.value
+            },
+            success: function (response) {
+                if (response == "Correct Password") {
+                    toastr["success"]("Logged in");
+                    window.location.href = "projects.php";
+                } else {
+                    toastr["warning"](response);
                 }
-            });
-        }
+            },
+            error: function () {
+                toastr["error"]("An error occurred...");
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+    document.getElementById("login-btn").onclick = login;
+});
+
+$(this).keydown(function (e) {
+    if (e.code == "Enter") {
+        login();
     }
 });
--- src/js/register.js
+++ src/js/register.js
@@ -1,56 +1,63 @@
-$(document).ready(function () {
-    document.getElementById("register-btn").onclick = function () {
+function register() {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const password2 = document.getElementById("password2");
 
-        const username = document.getElementById("username");
-        const password = document.getElementById("password");
-        const password2 = document.getElementById("password2");
+    if (username.value == "" || password.value == "" || password2.value == "" || password.value != password2.value) {
+        toastr["warning"]("Ensure you input a value in all fields!");
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/api/post_traffic.php",
+            data: {
+                action: 'register',
+                name: username.value,
+                pw: password.value
+            },
+            success: function (response) {
+                console.debug(response);
 
-        if (username.value == "" || password.value == "" || password2.value == "" || password.value != password2.value) {
-            toastr["warning"]("Ensure you input a value in all fields!");
-        } else {
-            $.ajax({
-                type: "POST",
-                url: "/api/post_traffic.php",
-                data: {
-                    action: 'register',
-                    name: username.value,
-                    pw: password.value
-                },
-                success: function (response) {
-                    console.debug(response);
+                if (response == "1") {
 
-                    if (response == "1") {
-
-                        $.ajax({
-                            type: "POST",
-                            url: "/api/post_traffic.php",
-                            data: {
-                                action: 'login',
-                                name: username.value,
-                                pw: password.value
-                            },
-                            success: function (response) {
-                                if (response == "Correct Password") {
-                                    toastr["success"]("Logged in");
-                                    window.location.href = "projects.php";
-                                } else {
-                                    toastr["warning"](response);
-                                }
-                            },
-                            error: function () {
-                                toastr["error"]("An error occurred...");
+                    $.ajax({
+                        type: "POST",
+                        url: "/api/post_traffic.php",
+                        data: {
+                            action: 'login',
+                            name: username.value,
+                            pw: password.value
+                        },
+                        success: function (response) {
+                            if (response == "Correct Password") {
+                                toastr["success"]("Logged in");
+                                window.location.href = "projects.php";
+                            } else {
+                                toastr["warning"](response);
                             }
-                        });
+                        },
+                        error: function () {
+                            toastr["error"]("An error occurred...");
+                        }
+                    });
 
-                    } else {
-                        toastr["warning"](response);
-                        toastr["warning"]("Please try again");
-                    }
-                },
-                error: function () {
-                    toastr["error"]("An error occurred");
+                } else {
+                    toastr["warning"](response);
+                    toastr["warning"]("Please try again");
                 }
-            });
-        }
+            },
+            error: function () {
+                toastr["error"]("An error occurred");
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+    document.getElementById("register-btn").onclick = register;
+});
+
+$(this).keydown(function (e) {
+    if (e.code == "Enter") {
+        register();
     }
 });
