44e881400446e183173f356ffa32e5c094c95b52
makl812c <makl812c@tu-dresden.de>
2025-10-16 09:28:41 +0200

    removed docker-compose

--- docker.sh
+++ docker.sh
@@ -111,25 +111,25 @@ if ! command -v docker &>/dev/null; then
 	curl -fsSL https://get.docker.com | bash
 fi
 
-if ! command -v docker-compose &>/dev/null; then
-	echo "docker-compose not found. Installing it..."
+#if ! command -v docker-compose &>/dev/null; then
+#	echo "docker-compose not found. Installing it..."
+#
+#	sudo apt update
 
-	sudo apt update
-
-	sudo apt-get install ca-certificates curl gnupg
-	sudo install -m 0755 -d /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	sudo chmod a+r /etc/apt/keyrings/docker.gpg
+#	sudo apt-get install ca-certificates curl gnupg
+#	sudo install -m 0755 -d /etc/apt/keyrings
+#	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#	sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 	# Add the repository to Apt sources:
-	echo \
-	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update
-
-	sudo apt install -y docker-compose
-fi
+#	echo \
+#	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+#	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#	sudo apt-get update
+#
+#	sudo apt install -y docker-compose
+#fi
 
 if ! command -v php >/dev/null; then
 	sudo apt update
